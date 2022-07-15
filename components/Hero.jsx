3859ca6180296338363fa747c0b45b72d34594f9
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-07-15 09:07:08 +0700

    Responsive Update (mostly)

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,45 +1,61 @@
 import { Logo } from "./Logo";
 import { Links } from "./Links";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 export const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-text">
-        <motion.div initial="hidden" animate="visible" transition={{duration: 1, delay:1}} variants={{
-          hidden: {
-            opacity: 0,
-            x:400
-          },
-          visible: {
-            opacity: 1,
-            x:0
-          }
-        }}>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 1, delay: 1 }}
+          variants={{
+            hidden: {
+              opacity: 0,
+              x: 400,
+            },
+            visible: {
+              opacity: 1,
+              x: 0,
+            },
+          }}
+        >
           <h1>Ahmad Zaki Akmal</h1>
           <p>Front End Web Developer</p>
           <div className="btn-container">
-            <a href="#">
-              <div className="btn">About Me</div>
-            </a>
-            <a href="#">
-              <div className="btn">Projects</div>
-            </a>
+            <Link href="/AboutMe" passHref>
+              <a href="#">
+                <div className="btn">About Me</div>
+              </a>
+            </Link>
+            <Link href="/Projects" passHref>
+              <a href="#">
+                <div className="btn">Projects</div>
+              </a>
+            </Link>
           </div>
         </motion.div>
       </div>
-      <motion.div className="hero-image"  initial="hidden" animate="visible" transition={{duration: 1}} variants={{
+      <motion.div
+        className="hero-image"
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 1 }}
+        variants={{
           hidden: {
             opacity: 0,
-            x:-400
+            x: -400,
           },
           visible: {
             opacity: 1,
-            x:0
-          }
-        }}>
-          <Logo/>
-        </motion.div>
+            x: 0,
+          },
+        }}
+      >
+        <Logo />
+      </motion.div>
     </div>
   );
 };
