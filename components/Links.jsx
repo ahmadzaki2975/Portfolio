a67da617380f8aacee78b7a6d45d43c7f6b93b3c
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-07-16 17:28:56 +0700

    Update on Links Component

--- components/Links.jsx
+++ components/Links.jsx
@@ -1,16 +1,19 @@
-import {FaInstagram, FaGithub, FaFacebook} from "react-icons/fa"
+import {FaInstagram, FaGithub, FaFacebook, FaLinkedin} from "react-icons/fa"
 
 export const Links = () => {
   return (
     <div className="links">
-      <a href="#">
+      <a href="https://www.instagram.com/ahmad._.zaki/">
         <div><FaInstagram/></div>
       </a>
-      <a href="#">
+      <a href="https://github.com/ahmadzaki2975">
         <div><FaGithub/></div>
       </a>
-      <a href="#">
+      <a href="https://www.facebook.com/profile.php?id=100014631301571">
         <div><FaFacebook/></div>
+      </a>
+      <a href="https://www.linkedin.com/in/ahmad-zaki-akmal/">
+        <div><FaLinkedin/></div>
       </a>
     </div>
   );
