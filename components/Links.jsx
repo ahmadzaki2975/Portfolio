47b686677dea2bf816cb0456d88219fba9c07500
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-02-07 08:54:56 +0700

    fix: 2023 update fix 1

--- components/Links.jsx
+++ components/Links.jsx
@@ -4,12 +4,20 @@ import { MdMail } from "react-icons/md";
 export const Links = () => {
   return (
     <div className="links">
-      <a href="https://www.instagram.com/ahmad._.zaki/" target="_blank">
+      <a
+        href="https://www.instagram.com/ahmad._.zaki/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <div>
           <FaInstagram />
         </div>
       </a>
-      <a href="https://github.com/ahmadzaki2975" target="_blank">
+      <a
+        href="https://github.com/ahmadzaki2975"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <div>
           <FaGithub />
         </div>
@@ -17,17 +25,26 @@ export const Links = () => {
       <a
         href="https://www.facebook.com/profile.php?id=100014631301571"
         target="_blank"
+        rel="noopener noreferrer"
       >
         <div>
           <FaFacebook />
         </div>
       </a>
-      <a href="https://www.linkedin.com/in/ahmad-zaki-akmal/" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/ahmad-zaki-akmal/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <div>
           <FaLinkedin />
         </div>
       </a>
-      <a href="mailto:ahmadzaki2975@gmail.com" target="_blank">
+      <a
+        href="mailto:ahmadzaki2975@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <div>
           <MdMail />
         </div>
