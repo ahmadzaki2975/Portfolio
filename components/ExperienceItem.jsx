887e7c15a5ce928af82ddca87e1f29a5f51b131f
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-02-07 08:38:51 +0700

    fix: rename file

--- components/ExperienceItem.jsx
+++ components/ExperienceItem.jsx
@@ -1,9 +1,9 @@
 export const ExperienceItem = ({ title, company, date, description }) => {
     return(
         <div className="experience-item">
-            <h2>{title}</h2>
+            <h2 className="experience-title">{title}</h2>
             <h3>{company}</h3>
-            <p>{date}</p>
+            <p className="experience-period">{date}</p>
             <p className="experience-desc">
               {description}
             </p>
