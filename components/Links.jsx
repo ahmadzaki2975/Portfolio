9746eb72028f0056c609a556aa196dc9800883b1
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-02-07 08:39:30 +0700

    fix: open link in new tab

--- components/Links.jsx
+++ components/Links.jsx
@@ -1,23 +1,36 @@
-import {FaInstagram, FaGithub, FaFacebook, FaLinkedin} from "react-icons/fa";
-import {MdMail} from "react-icons/md";
+import { FaInstagram, FaGithub, FaFacebook, FaLinkedin } from "react-icons/fa";
+import { MdMail } from "react-icons/md";
 
 export const Links = () => {
   return (
     <div className="links">
-      <a href="https://www.instagram.com/ahmad._.zaki/">
-        <div><FaInstagram/></div>
+      <a href="https://www.instagram.com/ahmad._.zaki/" target="_blank">
+        <div>
+          <FaInstagram />
+        </div>
       </a>
-      <a href="https://github.com/ahmadzaki2975">
-        <div><FaGithub/></div>
+      <a href="https://github.com/ahmadzaki2975" target="_blank">
+        <div>
+          <FaGithub />
+        </div>
       </a>
-      <a href="https://www.facebook.com/profile.php?id=100014631301571">
-        <div><FaFacebook/></div>
+      <a
+        href="https://www.facebook.com/profile.php?id=100014631301571"
+        target="_blank"
+      >
+        <div>
+          <FaFacebook />
+        </div>
       </a>
-      <a href="https://www.linkedin.com/in/ahmad-zaki-akmal/">
-        <div><FaLinkedin/></div>
+      <a href="https://www.linkedin.com/in/ahmad-zaki-akmal/" target="_blank">
+        <div>
+          <FaLinkedin />
+        </div>
       </a>
-      <a href="mailto:ahmadzaki2975@gmail.com">
-        <div><MdMail/></div>
+      <a href="mailto:ahmadzaki2975@gmail.com" target="_blank">
+        <div>
+          <MdMail />
+        </div>
       </a>
     </div>
   );
